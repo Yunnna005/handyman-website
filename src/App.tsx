88e5a81564bf5b44943d7cc6344535aaaa31cfb0
Yunnna005 <117185683+Yunnna005@users.noreply.github.com>
2025-04-18 15:03:26 +0100

    Header (#2)
    * Add DaisyUi plagin, rename index.css to style.css
    
    * V1_Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import NavBar from "./components/NavBar";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
-  );
+  return <NavBar />;
 }
 
 export default App;
