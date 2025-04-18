6b6365fcb25ab721768ab5038e0554f69e3a9e37
Yunnna005 <117185683+Yunnna005@users.noreply.github.com>
2025-04-18 18:14:40 +0100

    Content (#3)
    * V1_Hero, add tools.jpg image
    
    * V1 service section
    
    * V1 Gellary
    
    * V1 Footer
    
    * V2 Hero, Havbar. Change button color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import Footer from "./components/Footer";
+import Gellary from "./components/Gellary";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Services from "./components/Services";
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Services />
+      <Gellary />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
