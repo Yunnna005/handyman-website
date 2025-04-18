b66af8b62652e2b10919ca6c0132f9dad21488ec
Yunnna005 <annakovalenko0500@gmail.com>
2025-04-18 14:24:14 +0000

    V1_Hero, add tools.jpg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+export default function Hero() {
+  return (
+    <div
+      className="hero min-h-[31.25rem]"
+      style={{
+        backgroundImage: "url(/tools.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">WhatsApp</button>
+        </div>
+      </div>
+    </div>
+  );
+}
